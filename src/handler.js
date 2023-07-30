a3d40a5cb050fedeaaaa31ee9d64d5b892471d73
Fikri <fikrizhanking@gmail.com>
2023-07-30 10:15:46 +0700

    Change command order

--- src/handler.js
+++ src/handler.js
@@ -3,6 +3,6 @@ import { welcome, help, stats, enumDomain } from './controllers/main';
 export default function handler(bot) {
     bot.start(welcome);
     bot.help(help);
-    bot.on('text', enumDomain);
     bot.command('stats', stats);
+    bot.on('text', enumDomain);
 }
